fd8169da6297dd3314834fac9d9f3650c11220cf
dhshin04 <dhshin0301@gmail.com>
2023-06-10 10:47:05 +0900

    New Website Added

---
+++ javascript/home.js
@@ -0,0 +1,25 @@
+const menu = document.querySelector('.menu');
+const close = document.querySelector('.close');
+const nav = document.querySelector('nav');
+const menu_path = document.getElementById('menu-path');
+
+let count = 0;
+
+menu.addEventListener('click', () => {
+    if (count === 0) {
+        nav.classList.add('transition');
+        nav.classList.add('open-nav');
+        menu_path.setAttribute('d', 'm12 13.4l-4.9 4.9q-.275.275-.7.275q-.425 0-.7-.275q-.275-.275-.275-.7q0-.425.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7q0-.425.275-.7q.275-.275.7-.275q.425 0 .7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275q.425 0 .7.275q.275.275.275.7q0 .425-.275.7L13.4 12l4.9 4.9q.275.275.275.7q0 .425-.275.7q-.275.275-.7.275q-.425 0-.7-.275Z');
+        count++;
+    }
+    else {
+        nav.classList.add('transition');
+        nav.classList.remove('open-nav');
+        menu_path.setAttribute('d', 'M4 18q-.425 0-.712-.288Q3 17.425 3 17t.288-.712Q3.575 16 4 16h16q.425 0 .712.288q.288.287.288.712t-.288.712Q20.425 18 20 18Zm0-5q-.425 0-.712-.288Q3 12.425 3 12t.288-.713Q3.575 11 4 11h16q.425 0 .712.287q.288.288.288.713t-.288.712Q20.425 13 20 13Zm0-5q-.425 0-.712-.287Q3 7.425 3 7t.288-.713Q3.575 6 4 6h16q.425 0 .712.287Q21 6.575 21 7t-.288.713Q20.425 8 20 8Z');
+        count--;
+    }
+});
+
+nav.addEventListener("transitionend", function() {
+    nav.classList.remove('transition');
+});
